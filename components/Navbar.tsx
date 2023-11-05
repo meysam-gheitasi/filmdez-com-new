303a7945604b3856019db11017b55186eb2ec53d
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-11-05 19:57:37 +0330

    Entwicklung des Nav-Tags in der Navbar-Komponente

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex-center fixed top-0 z-50 border border-b-2 border-black-200 bg-black-100 text-white-800">
+      
+    </nav>
   )
 }
