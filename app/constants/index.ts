1e0273b203023e4e960420975dd6f8ec8c109d69
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-11-05 19:37:07 +0330

    Fügen Sie Projektressourcen zusammen mit der Adressdatei hinzu, um darauf zuzugreifen

---
+++ app/constants/index.ts
@@ -0,0 +1,54 @@
+// NAVIGATION
+export const NAV_ACCOUNT = [
+    { slug: '/login', key: 'singin_account', icon: '/assets/icons/navbar/singup.svg', title: 'حساب کاربری' },
+];
+
+export const NAV_MENU_MOVIES_FOREIGN = [
+    { slug: '/genre-all', key: 'movie_foreign', icon: '/assets/icons/navbar/arrowdown.svg', title: 'فیلم خارجی' },
+    { slug: '/genre-all/genre-drama', key: 'movie_genre_drama', title: 'درام' },
+    { slug: '/genre-all/genre-horror', key: 'movie_genre_horror', title: 'ترسناک' },
+    { slug: '/genre-all/genre-drama', key: 'movie_genre_drama', title: 'درام' },
+];
+
+export const NAV_MENU_IRANIAN = [
+    { slug: '/movies-iranian', key: 'movie_iranian', icon: '/assets/icons/navbar/arrowdown.svg', title: 'فیلم ایرانی' },
+    { slug: '/movies-iranian/movies', key: 'iranian_movies', title: 'سینمایی' },
+    { slug: '/movies-iranian/series', key: 'iranian_series', title: 'سریال' },
+];
+
+export const NAV_MENU_SERIES = [
+    { slug: '/series', key: 'series', icon: '/assets/icons/navbar/arrowdown.svg', title: 'خارجی سریال' },
+    { slug: '/series/indian', key: 'series_indian', title: 'هندی' },
+    { slug: '/series/american', key: 'series_american', title: 'امریکایی' },
+];
+
+export const SVG_NAVBAR = [
+    '/assets/icons/navbar/search.svg',
+]
+
+export const FOOTER_SOCIAL = [
+    '/assets/icons/footer/instagram.png',
+    '/assets/icons/footer/telegram.png',
+    '/assets/icons/footer/whatsapp.png',
+]
+
+export const FOOTER_CONFIRMATION = [
+    '/assets/icons/footer/confirmation.svg',
+    '/assets/icons/footer/telegram.svg',
+    '/assets/icons/footer/whatsapp.svg',
+]
+
+export const FOOTER_MENU = [
+    { slug: '/', key: 'home_footer', icon: '/assets/icons/footer/home.svg', title: 'خانه' },
+    { slug: '/genre-all', key: 'categories_footer', icon: '/assets/icons/footer/categories.svg', title: 'دسته بندی فیلم ها' },
+    { slug: '/singin', key: 'singin_footer', icon: '/assets/icons/footer/my_account.svg', title: 'حساب کاربری من' },
+]
+
+export const SVG_FOREIGN = [
+    '/assets/icons/IMDB.svg',
+]
+
+export const SVG_MOVIECARD = [
+    '/assets/icons/tag.svg',
+    '/assets/icons/calendar.svg',
+]
