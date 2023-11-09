4491394b9be03b550b7efaf9d04298d1bea04d34
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-11-09 17:32:35 +0330

    Korrektur der Schieberegleränderungen für die Hintergrundanzeige

--- components/sliders/TopSlider.tsx
+++ components/sliders/TopSlider.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Image from 'next/image';
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -6,92 +10,60 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import './styles.css';
-
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+
+import SwiperCore from 'swiper';
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 export default function TopSlider() {
     return (
         <>
-          <Swiper
-            style={{
-              '--swiper-navigation-color': '#fff',
-              '--swiper-pagination-color': '#fff',
-            }}
-            lazy={true}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Navigation]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-1.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-2.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-3.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-4.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-5.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-6.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-7.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-8.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://swiperjs.com/demos/images/nature-9.jpg"
-                loading="lazy"
-              />
-              <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-            </SwiperSlide>
-          </Swiper>
+            <Swiper
+                lazy={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+
+                <SwiperSlide>
+                    <Image className='!object-cover h-[600px] w-full'
+                        src="https://swiperjs.com/demos/images/nature-1.jpg"
+                        loading="lazy"
+                        alt='slieder image'
+                        width={1700}
+                        height={600}
+                    />
+                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image className='!object-cover h-[600px] w-full'
+                        src="https://swiperjs.com/demos/images/nature-3.jpg"
+                        loading="lazy"
+                        alt='slieder image'
+                        width={1700}
+                        height={600}
+                    />
+                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image className='!object-cover h-[600px] w-full'
+                        src="https://swiperjs.com/demos/images/nature-2.jpg"
+                        loading="lazy"
+                        alt='slieder image'
+                        width={1700}
+                        height={600}
+                    />
+                    <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                </SwiperSlide>
+
+            </Swiper >
         </>
-      );
-    }
-    
+    );
+}
