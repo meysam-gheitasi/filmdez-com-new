968d854e68c3b973e2d9dac8a1c7b8871dc37d05
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-11-12 17:45:52 +0330

    Verschieben Sie die Fußzeilenkomponenten in einen eigenen Ordner

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local'
 import './globals.css'
 
 import Navbar from '../components/navbar/Navbar'
-import Footer from '../components/Footer'
+import Footer from '../components/footer/Footer'
 
 
 
